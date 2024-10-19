32df43bd9da084db30e25293c75d54dd97536c43
Maciej Pyrc <maciekpyrc@gmail.com>
2024-10-19 15:20:06 +0200

    feat: 🎸 update the namespace

--- src/utils/get-position.utils.ts
+++ src/utils/get-position.utils.ts
@@ -2,7 +2,10 @@ export const getPosition = ({ scroll, height }: { scroll: number; height: number
   const position = Math.max(0, Math.min(scroll + window.innerHeight, height));
   const positionOut = Math.max(0, Math.min(scroll + window.innerHeight / 10, height));
   const positionScroll = Math.max(0, Math.min(scroll + window.innerHeight, window.innerHeight));
-  const positionScrollOut = Math.max(0, Math.min(scroll + window.innerHeight - height, window.innerHeight));
+  const positionScrollOut = Math.max(
+    0,
+    Math.min(scroll + window.innerHeight - height, window.innerHeight),
+  );
 
   return {
     position,
